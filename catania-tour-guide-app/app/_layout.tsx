841680e332a1aa8422b87c5ea2f45833dc1e7496
Lucio Alfio Leotta <lucio.a.leotta@gmail.com>
2025-04-09 23:20:58 +0200

    Refactor code structure for improved readability and maintainability

---
+++ catania-tour-guide-app/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router';
+import { Image, View } from 'react-native';
+import LanguageSwitcher from '../components/LanguageSwitcher';
+
+export default function Layout() {
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ flexDirection: 'row', alignItems: 'center', padding: 10, justifyContent: 'space-between' }}>
+        <Image source={require('../assets/images/logo.png')} style={{ width: 50, height: 50 }} resizeMode="contain" />
+        <LanguageSwitcher />
+      </View>
+      <Stack />
+    </View>
+  );
+}
